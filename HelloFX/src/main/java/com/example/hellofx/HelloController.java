6114efaee5a766b33da239bb4139b493a01b3bad
unknown <mafecala6@gmail.com>
2023-11-25 02:32:27 -0500

    se agrega cuarta escena

--- HelloFX/src/main/java/com/example/hellofx/HelloController.java
+++ HelloFX/src/main/java/com/example/hellofx/HelloController.java
@@ -183,10 +183,30 @@ public class HelloController
 
     public void clickFinalizar(ActionEvent event)
     {
-        //esto es para debug
-        System.out.println("no mi bro, aun no hace nada");
+        try
+        {
+            Parent root = FXMLLoader.load(getClass().getResource("fourthscene.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
+            stage.setScene(scene);
+            stage.show();
+
+
+
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+    public void clickButtonEnviarDay(ActionEvent event)
+    {
     }
 
+    public void clickButtonSiguienteDay(ActionEvent event)
+    {
+    }
     private Stage stage;
     private Scene scene;
     private Parent root;
